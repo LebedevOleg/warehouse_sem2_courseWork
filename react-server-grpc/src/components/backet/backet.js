dca2a64453b4345b6806bd444836c3be51c68f1e
LebedevOleg <56258311+LebedevOleg@users.noreply.github.com>
2023-05-24 13:08:01 +0300

    fix backet

--- react-server-grpc/src/components/backet/backet.js
+++ react-server-grpc/src/components/backet/backet.js
@@ -11,14 +11,35 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const BacketModal = () => {
 	const [open, setOpen] = React.useState(false);
-	const backet = JSON.parse(localStorage.getItem("backet"));
-
+	let b;
+	if (localStorage.getItem("backet") === null) {
+		b = [];
+		localStorage.setItem("backet", JSON.stringify([]));
+	} else {
+		b = JSON.parse(localStorage.getItem("backet"));
+	}
+	const backet = b;
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
 
 	const handleClick = () => {
 		window.location.replace("/backet");
 	};
+	if (backet.length === 0) {
+		<div>
+			<IconButton aria-label="backet" onClick={handleOpen}>
+				<ShoppingCartIcon />
+			</IconButton>
+			<Dialog open={open} onClose={handleClose}>
+				<DialogTitle>Корзина</DialogTitle>
+				<DialogContent>Нет товаров в корзине</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Закрыть</Button>
+					<Button onClick={handleClick}>Оформить</Button>
+				</DialogActions>
+			</Dialog>
+		</div>;
+	}
 
 	return (
 		<div>
